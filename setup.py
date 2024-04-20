09ca222cd53149a6ee2eac95c20853d5189bf25d
JiJunPark <162389990+JiJunPark@users.noreply.github.com>
2024-04-20 21:32:34 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,14 @@
+import setuptools
 
+setuptools.setup(
+    name='Parkjijun',
+    version='0.0.1',
+    description='nice calculator',
+    author='jjun2859',
+    url ='https://github.com/JiJunPark/calc0001'
+    download_url='https://github.com/JiJunPark/calc0001/'
+    packages==['mycalc'],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+    ],
+)
